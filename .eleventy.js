8fc0eee02755d3382d3204f47927b6cede5089aa
mrmicaiah <mrmicaiah@gmail.com>
2026-03-06 15:42:32 -0600

    Filter out future-dated posts from blog listing

--- .eleventy.js
+++ .eleventy.js
@@ -14,9 +14,12 @@ module.exports = function(eleventyConfig) {
   });
   
   eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/blog/*.md").sort((a, b) => {
-      return b.date - a.date;
-    });
+    const now = new Date();
+    return collectionApi.getFilteredByGlob("src/blog/*.md")
+      .filter(post => post.date <= now)
+      .sort((a, b) => {
+        return b.date - a.date;
+      });
   });
 
   return {
